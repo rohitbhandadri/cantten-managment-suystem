78bdeeb5e8da3b6bdab22ade4c3571fe0daf633f
rohitbhandari <okrohit409@gmail.com>
2026-09-08 05:39:05 +0545

    Update orders.php

--- canteen-management-system/views/customer/orders.php
+++ canteen-management-system/views/customer/orders.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../../controllers/OrderController.php';
-requireLogin();
+requireCustomer();
 
 $database = new Database();
 $db = $database->connect();
