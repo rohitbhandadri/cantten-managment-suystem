c23f196f11c2e4a4fe8db00f768db52a7a29beb7
rohitbhandari <okrohit409@gmail.com>
2026-09-08 05:41:38 +0545

    Update dashboard.php

--- canteen-management-system/views/staff/dashboard.php
+++ canteen-management-system/views/staff/dashboard.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../../models/StaffWorkspace.php';
 requireStaff();
 
+redirect(staffDashboardPath());
+
 $database = new Database();
 $db = $database->connect();
 $workspace = new StaffWorkspace($db);
