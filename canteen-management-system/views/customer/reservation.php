5cb551de165f144c68ae22f10b667b045fd63ab9
rohitbhandari <okrohit409@gmail.com>
2026-09-08 05:39:10 +0545

    Update reservation.php

--- canteen-management-system/views/customer/reservation.php
+++ canteen-management-system/views/customer/reservation.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../../controllers/ReservationController.php';
-requireLogin();
+requireCustomer();
 
 $database = new Database();
 $db = $database->connect();
