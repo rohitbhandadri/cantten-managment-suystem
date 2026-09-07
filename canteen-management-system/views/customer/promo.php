f04e3a37edebc435a03e766c50ad0482b45291ec
rohitbhandari <okrohit409@gmail.com>
2026-09-08 05:39:52 +0545

    Update promo.php

--- canteen-management-system/views/customer/promo.php
+++ canteen-management-system/views/customer/promo.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../../models/Promo.php';
-requireLogin();
+requireCustomer();
 
 $database = new Database();
 $db = $database->connect();
