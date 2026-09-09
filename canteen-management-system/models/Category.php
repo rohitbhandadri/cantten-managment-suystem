169bd920579e3617c99045ba89878b96efb1db25
bhumi123-beep <thagunnabhumi123@gmail.com>
2026-09-09 08:16:07 +0545

    Update Category.php

--- canteen-management-system/models/Category.php
+++ canteen-management-system/models/Category.php
@@ -18,4 +18,10 @@ class Category {
         $stmt->execute([$name]);
         return $this->conn->lastInsertId();
     }
+
+    public function exists($id) {
+        $stmt = $this->conn->prepare("SELECT id FROM {$this->table} WHERE id = ? LIMIT 1");
+        $stmt->execute([(int)$id]);
+        return (bool)$stmt->fetchColumn();
+    }
 }
